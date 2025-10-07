954f031f83e31da5708d2fa5a6ca32ad69106412
samuel <samueldaza820@gmail.com>
2025-10-07 10:11:36 -0500

    #61 feat: Crear eñ

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import helmet from "helmet";
 import cors from "cors";
 import authRoutes from "./routes/auth.routes";
+import usersRoutes from "./routes/users.routes"
 
 const app = express();
 
@@ -10,5 +11,5 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/auth", authRoutes);
-
+app.use("/users", usersRoutes);
 export default app;
